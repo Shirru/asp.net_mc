d7d6788973fd3478ec5e018b720ebd19ceac4bc6
Shirru <annafreia@mail.ru>
2016-03-10 22:56:26 +0300

    Домашнее задание №2. Задача про Гарри и Ларри

---
+++ homework02/task01/task01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задача про Гарри и Ларри
+            Console.WriteLine("Введите 2 числа — количество банок, простреленных Гарри и Ларри соответственно (через пробел):");
+            var cans = Console.ReadLine().Split(' ');
+            var all = Convert.ToInt32(cans[0]) + Convert.ToInt32(cans[1]) - 1;
+            Console.WriteLine("Ответ: {0} {1}",(all - Convert.ToInt32(cans[0])),(all - Convert.ToInt32(cans[1])));
+            Console.ReadLine();
+        }
+    }
+}
