06a33fb3726299e4c0f087aad052b05f812d5620
Shirru <annafreia@mail.ru>
2016-03-11 00:05:46 +0300

    дз 2, сумма от 1 до n

---
+++ homework02/task03/task03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Сумма от 1 до n
+            Console.WriteLine("Введите n <= 10000");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (Math.Abs(n) <= 10000)
+            {
+                if(n > 0)
+                    Console.WriteLine("Ответ: " + (n*(n + 1))/2);
+                else
+                    Console.WriteLine("Ответ: " + (-(n * (n - 1)) / 2 + 1));
+            }
+            else Console.WriteLine("Некорректный ввод");
+            Console.ReadLine();
+        }
+    }
+}
